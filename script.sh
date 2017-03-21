7396c9da30d49ac4b7feb730587c67752cce4863
Mahsa Golchian <mahsa.gol89@gmail.com>
2017-03-21 17:42:20 +0330

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,3 +1,7 @@
+#This code has been used by me to rename multiples files in a folder into sequential numbers. 
+#Created by Mahsa Golchian
+##!/bin/sh — Execute the file using sh, the Bourne shell, or a compatible shell
+#chmod 755 or chmod +x just like the #!/bin/sh at the begining of the script.
 counter=1
 for i in *.ogg; do
   new=$(printf "%03d.ogg" "$counter") #03 pad to length of 3
