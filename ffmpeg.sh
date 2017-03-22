47acb7a69cbeb53dca7999824fa752c12b72c9e6
Mahsa Golchian <mahsa.gol89@gmail.com>
2017-03-22 12:54:51 +0430

    Update ffmpeg.sh

--- ffmpeg.sh
+++ ffmpeg.sh
@@ -1,6 +1,7 @@
-# whenever you want to convert multiple files in a folder into another format you need this!
+# whenever you want to convert multiple files in a folder into another format then you need this!
 # created by Mahsa Golchian
-# 
+##!/bin/sh — Execute the file using sh, the Bourne shell, or a compatible shell
+#chmod 755 or chmod +x just like the #!/bin/sh at the begining of the script.
 counter=1
 for i in *.m4a; do
   new=$(printf "%03d.mp3" "$counter") #03 pad to length of 3
