97c8601fdddf7f754b1fe06027cabe3c8cf0c698
Mahsa Golchian <mahsa.gol89@gmail.com>
2017-03-24 20:37:39 +0430

    Create mping.sh

---
+++ mping.sh
@@ -0,0 +1,15 @@
+#When you want multiple ip address via ping, seeing the output you should run $ cat /path/to/log.txt. 
+#write your ip addresses in a file just like list.txt 
+
+#!/bin/bash
+# Program name: mping.sh
+date
+cat /path/to/list.txt |  while read output
+do
+    ping -c 1 "$output" > /dev/null
+    if [ $? -eq 0 ]; then
+    echo "node $output is up" 
+    else
+    echo "node $output is down"
+    fi
+done
