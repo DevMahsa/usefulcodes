5f86fb1490991e8e7408b1ade5a0444e7d77924a
Mahsa Golchian <mahsa.gol89@gmail.com>
2017-03-27 19:18:23 +0430

    Update mfind.sh

--- mfind.sh
+++ mfind.sh
@@ -1,3 +1,7 @@
+#This code has been used by me to find multiples files in a folder with specific extension. 
+#Created by Mahsa Golchian
+##!/bin/sh — Execute the file using sh
+#chmod 755 or chmod +x just like the #!/bin/sh at the begining of the script.
 find . -name '*.webm' -print0 |
     while IFS= read -r -d $'\0' line; do
         echo $line
