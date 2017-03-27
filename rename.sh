afb59ef7ca1e9078a8c20de19316ae1770d687e2
Mahsa Golchian <mahsa.gol89@gmail.com>
2017-03-27 19:18:52 +0430

    Update rename.sh

--- rename.sh
+++ rename.sh
@@ -1,6 +1,6 @@
 #This code has been used by me to rename multiples files in a folder into sequential numbers. 
 #Created by Mahsa Golchian
-##!/bin/sh — Execute the file using sh, the Bourne shell, or a compatible shell
+##!/bin/sh — Execute the file using sh
 #chmod 755 or chmod +x just like the #!/bin/sh at the begining of the script.
 counter=1
 for i in *.ogg; do
