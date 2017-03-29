9b5fd9bcd2cd026c0e59827521a0cf5ced858236
Mahsa Golchian <mahsa.gol89@gmail.com>
2017-03-30 01:28:37 +0430

    Update mopen.sh

--- mopen.sh
+++ mopen.sh
@@ -2,6 +2,8 @@
 # sleep time can be changed by every single needs
 # created by Mahsa Golchian 
 # this code is running in mac terminal 
+##!/bin/sh — Execute the file using sh, the Bourne shell, or a compatible shell
+#chmod 755 or chmod +x just like the #!/bin/sh at the begining of the script.
 counter=1
 for i in *.ogg; do
   open  "$i"
